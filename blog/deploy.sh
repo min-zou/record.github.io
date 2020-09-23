cbf810717a261ee23419268de6a1329bb4336df8
xiao <1120120667@bit.edu.cn>
2020-09-23 15:11:06 +0800

    [1]remove aliyun link

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -1,12 +1,12 @@
 rm -rf public
 
 find -type d -empty -exec rm -rf {} \;
-# hugo --theme=hello-friend-ng --baseUrl="https://min-zou.github.io/record.github.io"
-hugo --theme=hello-friend-ng --baseUrl="https://www.robot-rex.top"
-cp CNAME public/
+hugo --theme=hello-friend-ng --baseUrl="https://min-zou.github.io/record.github.io"
+# hugo --theme=hello-friend-ng --baseUrl="https://www.robot-rex.top"
+# cp CNAME public/
 cd public
 git init
 git add .
-git config user.email "xxiao_l@163.com"
+git config --local user.email "xxiao_l@163.com"
 git commit -m "build site"
 git push -f git@github.com:min-zou/record.github.io.git master
